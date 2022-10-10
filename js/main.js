4c6a13e16d1fad64498778612bbd784b55239d97
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-10-10 07:00:42 -0700

    Done with experience

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-// Bootstrap Components
-// This affects the carousel on the review section; I changed the class names but to no avail, kindly help look into it.
+//Testimonial Slider
 
 const track = document.querySelector('.track')
 const slides = Array.from(track.children)
@@ -52,7 +51,38 @@ function updateSlide(transitDot = btns[targetIndex]) {
 timer = setInterval(() => {
     nextBtn.click()
 }, 6000)
-$('.carousel').carousel()
+
+
+
+//Experience Statistics counter
+const clients = document.querySelector(".clients")
+const quality = document.querySelector(".quality")
+const missions = document.querySelector(".missions")
+
+let counterClient = 0;
+let counterQuality = 0;
+let counterMissions = 0;
+
+
+const clientTimer = setInterval(() => {
+    counterClient++;
+    clients.textContent = `${counterClient}`
+    if(counterClient == 570) clearInterval(clientTimer)
+}, 10);
+
+const qualityTimer = setInterval(() => {
+    counterQuality++;
+    quality.textContent = `${counterQuality}%`
+    if(counterQuality == 100) clearInterval(qualityTimer)
+}, 10);
+
+const missionTimer = setInterval(() => {
+    counterMissions++;
+    missions.textContent = `${counterMissions}+`
+    if(counterMissions == 957) clearInterval(missionTimer)
+}, 10);
+
+
 
 
 // For the accordion section 
@@ -61,6 +91,9 @@ $(document).ready(function () {
     $(".collapse.show").each(function () {
         $(this).prev(".card-header").find(".fa").addClass("fa-minus").removeClass("fa-plus");
     });
+
+    //Bootstrap Carousel slider
+    $('.carousel').carousel()
 
     // Toggle right and down arrow icon on show hide of collapse element
     $(".collapse").on('show.bs.collapse', function () {
